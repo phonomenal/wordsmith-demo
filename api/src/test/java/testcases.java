d842c634bd0580cb7700dd446d2840e3a9d6b69c
James Ha <james-ha@Jamess-MacBook-Pro.local>
2024-02-07 14:40:20 -0500

    updating test cases

--- api/src/test/java/testcases.java
+++ api/src/test/java/testcases.java
@@ -1,17 +1,20 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
- 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 class JUnit5TestCases {
  
     @DisplayName("Single test successful")
     @Test
     void testSingleSuccessTest() {
         System.out.println("Successful use case 1");
+        assertEquals(1,1);
     }
 
-    @DisplayName("Single test successful")
+    @DisplayName("Single word successful")
     @Test
-    void testWordGeneratorTest() {
-        System.out.println("Random");
+    void testWordSuccessTest() {
+        System.out.println("Successful use case 1");
     }
 }
